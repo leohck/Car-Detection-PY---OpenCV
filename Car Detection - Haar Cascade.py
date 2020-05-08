cb32e2fc112f100d9e41bdb0108f2dd0ca65a4fe
Leonardo Black <leonardoblack.net@gmail.com>
2020-05-08 18:20:51 -0300

    Update file

--- Car Detection - Haar Cascade.py
+++ Car Detection - Haar Cascade.py
@@ -3,47 +3,97 @@ import time
 import numpy as np
 from datetime import datetime
 
-def main(args):
-    car_classificador = cv2.CascadeClassifier('cascades/haarcascade_car.xml')
-    camera = cv2.VideoCapture('imagens/video.mp4')
-    camera_port = 0
-    nFrames = 30
-    file = "imagens/imagenTeste.png"
-    emLoop = True
+DATE_FORMAT = "%d/%m/%Y"
+TIME_FORMAT = "%H:%M:%S"
+SHORT_DATETIME_FORMAT = "%d/%m/%Y %H:%M:%S"
 
-    print
-    "Digite <ESC> para sair / <s> para Salvar"
 
-    while (emLoop):
+def pegar_data_hora_atual():
+    atual = datetime.now()
+    data = atual.date().strftime(DATE_FORMAT)
+    hora = atual.time().strftime(TIME_FORMAT)
+    data_hora = atual.strftime(SHORT_DATETIME_FORMAT)
+    return data, hora, data_hora
 
+
+def tirar_foto(frame, dimensoes, file_name):
+    (x, y, w, h) = dimensoes[0], dimensoes[1], dimensoes[2], dimensoes[3]
+    file = f"imagens/{file_name}.png"
+    imagem = frame[y:y+h, x:x+w]
+    cv2.namedWindow("foto", cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('foto', imagem)
+    key = cv2.waitKey(0) & 0xFF
+    if key == ord('e'):
+        cv2.destroyWindow("foto")
+    elif key == ord('s'):
+        cv2.imwrite(file, imagem)
+        cv2.destroyWindow("foto")
+        print(f'foto salva -> {file_name}')
+
+
+def main():
+    car_classificador = cv2.CascadeClassifier('cascades/cars2.xml')
+    camera = cv2.VideoCapture('videos/rodovia.mp4')
+    reproduzir = False
+    detectar_carros = False
+    while True:
         retval, frame = camera.read()
+        frame = cv2.resize(frame, None, fx=.5, fy=.5)
+
+        width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+        height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        frame = cv2.rectangle(frame, (0, 0), (int(width), int(height/15)), (255, 255, 255), -1)
+        comandos = "s - iniciar | p - pausar | e - encerrar"
+        comandos2 = "d-detectar/pausar | f-foto (e-cancelar|s-salvar)"
+        font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        cv2.putText(frame, comandos, (10, int(height/40)), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(frame, comandos2, (10, int(height/18)), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        carros = car_classificador.detectMultiScale(gray, 1.4, 2)
 
-        for (x, y, w, h) in carros:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.imshow('Detector de carros', frame)
+        if not reproduzir:
+            key = cv2.waitKey(0) & 0xFF
+            if key == ord('s'):
+                print('tecla s -> reprodução do video iniciada')
+                reproduzir = True
+            elif key == ord('e'):
+                print('tecla e -> reprodução do video encerrada')
+                break
+        else:
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('p'):
+                print('tecla p -> reprodução do video pausada')
+                reproduzir = False
 
-        k = cv2.waitKey(100)
-        if k == 27:
-            emLoop = False
+            if key == ord('d'):
+                if detectar_carros:
+                    print('tecla d -> detecção de veiculos encerrada')
+                    detectar_carros = False
+                else:
+                    print('tecla d -> detecção de veiculos iniciada')
+                    detectar_carros = True
 
-        elif k == ord('s'):
-            now = datetime.now()
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            #print(now)
-            #frame = cv2.putText(frame, now, (10, 500), font, 4, (255, 0, 0), 2, cv2.LINE_AA)
-            cv2.imwrite(file, frame)
-            emLoop = True
+            if key == ord('e'):
+                print('tecla e -> reprodução do video encerrada')
+                break
 
-          if print == 's':
-            cv2.imshow('fototirada', frame)
+            if detectar_carros:
+                carros = car_classificador.detectMultiScale(gray, 1.4, 2)
+                for carro in carros:
+                    if carro.all():
+                        (x, y, w, h) = carro[0], carro[1], carro[2], carro[3]
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+                        horario = pegar_data_hora_atual()
+                        cv2.putText(frame, horario[2], (int(x/2), y), font, 1, (255, 255, 0), 1, cv2.LINE_AA)
+                        key = cv2.waitKey(1)
+                        if key == ord('f'):
+                            tirar_foto(frame, carro, x)
 
-    cv2.destroyAllWindows()
+        cv2.imshow('Detector de carros', frame)
+
     camera.release()
-    return 0
+    cv2.destroyAllWindows()
 
-if __name__ == '__main__':
-    import sys
 
-sys.exit(main(sys.argv))
+main()
+# print(pegar_data_hora_atual())
